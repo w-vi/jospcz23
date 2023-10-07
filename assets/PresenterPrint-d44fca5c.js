7e78c310b231b0ea79ee99269233df1ff0cf859e
w-vi <vilibald@wvi.cz>
2023-10-07 14:52:58 +0200

    Publish

---
+++ assets/PresenterPrint-d44fca5c.js
@@ -0,0 +1,17 @@
+import{d as _,u as d,a as p,c as m,b as h,r as u,e as a,f as t,t as s,g as r,F as f,h as g,n as v,i as x,o as n,j as y,k as b,l as N,m as k,_ as w}from"./index-9f52a6f9.js";import{N as P}from"./NoteDisplay-28ec416a.js";const V={class:"m-4"},L={class:"mb-10"},S={class:"text-4xl font-bold mt-2"},T={class:"opacity-50"},j={class:"text-lg"},z={class:"font-bold flex gap-2"},B={class:"opacity-50"},D=t("div",{class:"flex-auto"},null,-1),H={key:0,class:"border-gray-400/50 mb-8"},C=_({__name:"PresenterPrint",setup(F){d(`
+@page {
+  size: A4;
+  margin-top: 1.5cm;
+  margin-bottom: 1cm;
+}
+* {
+  -webkit-print-color-adjust: exact;
+}
+html,
+html body,
+html #app,
+html #page-root {
+  height: auto;
+  overflow: auto !important;
+}
+`),p({title:`Notes - ${m.title}`});const c=h(()=>u.map(o=>{var l;return(l=o.meta)==null?void 0:l.slide}).filter(o=>o!==void 0&&o.noteHTML!==""));return(o,l)=>(n(),a("div",{id:"page-root",style:v(r(x))},[t("div",V,[t("div",L,[t("h1",S,s(r(m).title),1),t("div",T,s(new Date().toLocaleString()),1)]),(n(!0),a(f,null,g(c.value,(e,i)=>(n(),a("div",{key:i,class:"flex flex-col gap-4 break-inside-avoid-page"},[t("div",null,[t("h2",j,[t("div",z,[t("div",B,s(e==null?void 0:e.no)+"/"+s(r(y)),1),b(" "+s(e==null?void 0:e.title)+" ",1),D])]),N(P,{"note-html":e.noteHTML,class:"max-w-full"},null,8,["note-html"])]),i<c.value.length-1?(n(),a("hr",H)):k("v-if",!0)]))),128))])],4))}}),E=w(C,[["__file","/home/wvi/src/jospcz23/node_modules/@slidev/client/internals/PresenterPrint.vue"]]);export{E as default};
